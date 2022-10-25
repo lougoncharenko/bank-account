8472beb1ea6f51e920df8e33c4b134e8c21c2674
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-10-24 20:50:41 -0400

    create BankAccount class with three attributes

---
+++ BankAccount.py
@@ -0,0 +1,6 @@
+#create BankAccount class
+class BankAccount():
+    def __init__(self, full_name, account_number, balance):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.balance = balance 
