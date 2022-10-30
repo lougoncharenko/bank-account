424ae0c3d03fd4b58c31c9832ac5cb21a3e883aa
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-10-30 14:26:21 -0400

    created a function that generates a random 8-digit account number

--- BankAccount.py
+++ BankAccount.py
@@ -1,9 +1,17 @@
 #create BankAccount class
+from random import randint
+
 class BankAccount():
-    def __init__(self, full_name, account_number, balance):
+    def __init__(self, full_name):
         self.full_name = full_name
-        self.account_number = account_number
+        self. account_number = self.random_account_number(8)
         self.balance = 0
+
+    def random_account_number(self, n):
+        range_start = 10**(n-1)
+        range_end = (10**n)-1
+        self.account_number = randint(range_start, range_end)
+        return self.account_number
 
     def deposit(self, amount):
         """
@@ -56,7 +64,7 @@ class BankAccount():
         print(f'{self.full_name}\n {self.account_number}\n {self.balance}')
       
 #Define 3 different bank account examples using the BankAccount() object.
-John_account = BankAccount('John Thomas', 10000001, 0)
+John_account = BankAccount('John Thomas')
 John_account.deposit(700)
 John_account.print_statement()
 John_account.deposit(800)
